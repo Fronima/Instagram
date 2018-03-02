846d0d2d94a414a81175a39f9934b37e57626b25
Fronima <frankcoleman55@gmail.com>
2018-03-01 19:19:15 -0500

    cleaned

--- Instagram/ViewControllers/ComposeViewController.swift
+++ Instagram/ViewControllers/ComposeViewController.swift
@@ -7,21 +7,56 @@
 //
 
 import UIKit
+import Parse
+import  RSKPlaceholderTextView
 
-class ComposeViewController: UIViewController {
+class ComposeViewController: UIViewController , UIImagePickerControllerDelegate, UINavigationControllerDelegate{
 
+    @IBOutlet weak var imageview: UIImageView!
+    @IBOutlet weak var comments: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func share(_ sender: Any) {
+        let post = Gram()
+        post.postUserImage(image: imageview.image!, withCaption: comments.text, completion: {(state: Bool, error:Error?) in
+            if state {
+                print("post uploaded")
+            }else{
+                print(error?.localizedDescription)
+            }
+        })
+        self.navigationController?.popViewController(animated: true)
+    }
     
-
+    
+    
+    @IBAction func takePicture(_ sender: Any) {
+        let vc = UIImagePickerController()
+        vc.delegate = self
+        vc.allowsEditing = true
+        vc.sourceType = .camera
+        self.present(vc, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        let image = info[UIImagePickerControllerEditedImage] as! UIImage
+        imageview.image = image
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
     /*
     // MARK: - Navigation
 
--- Instagram/ViewControllers/HomeFeedViewController.swift
+++ Instagram/ViewControllers/HomeFeedViewController.swift
@@ -7,13 +7,28 @@
 //
 
 import UIKit
+import Parse
 
-class HomeFeedViewController: UIViewController {
+class HomeFeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+
+    
+    var InstaPosts: [AnyObject] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.delegate = self
+        tableView.dataSource = self
         // Do any additional setup after loading the view.
+        getPhotos()
+        let refresh = UIRefreshControl()
+        refresh.addTarget(self, action: #selector(refreshControlAction(_:)), for: UIControlEvents.valueChanged)
+        //tableView.insertSubview(refresh, at: 0)
+        tableView.estimatedRowHeight = 400
+        tableView.refreshControl = refresh
+        tableView.autoresizesSubviews = true
+        tableView.rowHeight = UITableViewAutomaticDimension
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,7 +36,58 @@ class HomeFeedViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    
+    @objc func refreshControlAction(_ refreshControl: UIRefreshControl){
+        getPhotos()
+        refreshControl.endRefreshing()
+    }
+    
+    func getPhotos(){
+        let query = PFQuery(className: "Gram")
+        query.limit = 20
+        query.addDescendingOrder("createdAt")
+        query.findObjectsInBackground(block: {(posts: [PFObject]?, error: Error?) in
+            if error == nil{
+                self.InstaPosts = posts!
+                self.tableView.reloadData()
+            }else{
+                print(error?.localizedDescription)
+            }
+        })
+        
+        
+    }
+    
+    
+    @IBAction func logout(_ sender: Any) {
+        PFUser.logOut()
+        self.performSegue(withIdentifier: "logoutSegue", sender: sender)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return InstaPosts.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "InstaCell") as! InstaCell
+        let post = InstaPosts[indexPath.row] as! Gram
+        cell.comment.text = post.caption
+        cell.dateTime.text = post.createdAt?.description
+        do{
+            try cell.instaImage.image = UIImage(data: post.media.getData())
+        }catch {
+            print(error.localizedDescription )
+        }
+        
+        
+        
+        return cell
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        
+        tableView.beginUpdates()
+        tableView.endUpdates()
+    }
     /*
     // MARK: - Navigation
 
